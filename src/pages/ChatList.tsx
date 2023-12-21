c3bf5b2d304850c7060031790af6c1e7b4d0bcad
johnnithin08 <johnnithin08@gmail.com>
2023-12-21 12:46:02 +0000

    coommit

--- src/pages/ChatList.tsx
+++ src/pages/ChatList.tsx
@@ -1,21 +1,31 @@
 import React, { Fragment, useEffect, useState } from 'react'
-import { Button, Image, Pressable, RefreshControl, Text, View } from 'react-native'
+import { Button, Image, Pressable, RefreshControl, Text, View, ViewStyle, useWindowDimensions } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { ChatRoomItem } from "../components"
-import { colorWhite, flexChild } from '../styles'
+import { centerVertical, colorGray, colorWhite, flexChild, flexRow } from '../styles'
 import { FlatList } from 'react-native-gesture-handler'
 import { useNavigation } from '@react-navigation/native'
 import { useAuthenticator } from '@aws-amplify/ui-react-native'
 import { generateClient } from 'aws-amplify/api';
 import { getCurrentUser } from 'aws-amplify/auth';
 import { listChatRooms } from '../graphql/customQueries'
+import Feather from 'react-native-vector-icons/Feather'
 
 export const ChatList = () => {
     const navigation = useNavigation()
+    const { width } = useWindowDimensions();
     const { signOut } = useAuthenticator();
     const [chatRooms, setChatRooms] = useState([])
     const [loading, setLoading] = useState<boolean>(false)
     const client = generateClient()
+    const icon: ViewStyle = {
+        marginHorizontal: 20,
+    }
+
+
+    const handleUsers = () => {
+        navigation.navigate("Users")
+    }
 
     const fetchChatRooms = async () => {
         try {
@@ -40,6 +50,24 @@ export const ChatList = () => {
     }, [])
     return (
         <SafeAreaView style={{ ...flexChild, backgroundColor: colorWhite._1 }}>
+            <View>
+                <View style={{
+                ...flexRow,
+                width: width,
+                padding: 10,
+                ...centerVertical
+            }}>
+                <Image
+                    source={{ uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg" }}
+                    style={{
+                        height: 30,
+                        width: 30,
+                        borderRadius: 30
+                    }} />
+                <Text style={{ ...flexChild, fontWeight: "bold", marginLeft: 20 }}>Home</Text>
+                <Feather name="edit-2" onPress={handleUsers} size={24} color={colorGray._5} style={icon} />
+            </View>
+            </View>
             {chatRooms.length > 0 ? (
 
                 <FlatList
